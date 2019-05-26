ab4295c9483ec4fbe552ede256bab73a1df21a5f
Víctor Lucía Gómez <tombombadilbridport@gmail.com>
2019-05-26 11:00:00 +0200

    Add files via upload

---
+++ SENSORS/KEYES KS0349/keyes1.ino
@@ -0,0 +1,13 @@
+
+int led = 2;
+ void setup()
+{
+  pinMode(led, OUTPUT);     //Set Pin3 as output
+}
+void loop()
+{
+          digitalWrite(led, HIGH);   //Turn on led
+          delay(2000);
+          digitalWrite(led, LOW);    //Turn off led
+          delay(2000);
+}
